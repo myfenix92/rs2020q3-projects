6dabd422651e4666d5ece7c66b22f0af366995d5
Yana Kornejkova <bloom.hog@gmail.com>
2020-11-08 01:02:37 +0300

    feat: add drag&drop

--- gem-puzzle/src/js/index.js
+++ gem-puzzle/src/js/index.js
@@ -32,18 +32,20 @@ document.querySelector('.pause_btn').addEventListener('click', () => {
 class CreatePuzzle {
   constructor() {
     this.mainBox = document.createElement('div');
+    this.main = document.createElement('main');
     this.box = this.mainBox.querySelectorAll('.box');
     this.arrayBoxes = [];
     this.fragment = [];
     this.randomArray = [];
-    this.level = 3;
+    this.level = 4;
     this.activeBox = 0;
   }
 
   init() {
     this.mainBox.classList.add('main_box');
     this.mainBox.append(...this.createBoxes());
-    document.body.append(this.mainBox);
+    this.main.append(this.mainBox);
+    document.body.append(this.main);
   }
 
   getRandom() {
@@ -55,7 +57,7 @@ class CreatePuzzle {
 
   getDefaultArray() {
     this.arrayBoxes = [];
-    for (let i = 1; i <= this.level ** 2; i++) {
+    for (let i = 1; i <= this.level ** 2; i += 1) {
       if (i === this.level ** 2) {
         this.arrayBoxes.push(0);
       } else {
@@ -67,7 +69,7 @@ class CreatePuzzle {
 
   getRandomArray() {
     this.randomArray = [];
-    for (let i = 0; i < this.level ** 3; i++) {
+    for (let i = 0; i < this.level ** 3; i += 1) {
       this.randomArray.push(this.getRandom());
     }
     this.randomArray = [...new Set(this.randomArray.concat(this.arrayBoxes))];
@@ -78,7 +80,7 @@ class CreatePuzzle {
     this.getDefaultArray();
     this.getRandomArray();
 
-    for (let i = 0; i < this.randomArray.length; i++) {
+    for (let i = 0; i < this.randomArray.length; i += 1) {
       if (this.randomArray[i] === 0) {
         this.boxElement = document.createElement('div');
         this.boxElement.classList.add('box', 'box_empty');
@@ -110,7 +112,7 @@ class Move {
   activeBoxes(event) {
     if (event.target.closest('.box')) {
       this.activeBox = event.target.closest('.box').dataset.num - 0;
-      for (let i = 0; i < createPuzzle.randomArray.length; i++) {
+      for (let i = 0; i < createPuzzle.randomArray.length; i += 1) {
         if (createPuzzle.randomArray[i] === this.activeBox) {
           this.activeBox = i;
           return this.activeBox;
@@ -120,48 +122,103 @@ class Move {
   }
 
   moveBoxes() {
-    this.numBufferBox = 0;
-    if (createPuzzle.randomArray[this.activeBox + 1] === 0
+    if (checkEmptyBox) {
+      this.numBufferBox = 0;
+      if (createPuzzle.randomArray[this.activeBox + 1] === 0
       && (this.activeBox + 1) % createPuzzle.level !== 0) {
-      document.querySelectorAll('.box')[this.activeBox + 1].after(document.querySelectorAll('.box')[this.activeBox]);
-      this.numBufferBox = createPuzzle.randomArray[this.activeBox];
-      createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox + 1];
-      createPuzzle.randomArray[this.activeBox + 1] = this.numBufferBox;
-      this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
-    } else if (createPuzzle.randomArray[this.activeBox - 1] === 0
+        document.querySelectorAll('.box')[this.activeBox + 1].after(document.querySelectorAll('.box')[this.activeBox]);
+        this.numBufferBox = createPuzzle.randomArray[this.activeBox];
+        createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox + 1];
+        createPuzzle.randomArray[this.activeBox + 1] = this.numBufferBox;
+        this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
+      } else if (createPuzzle.randomArray[this.activeBox - 1] === 0
       && (this.activeBox) % createPuzzle.level !== 0) {
-      document.querySelectorAll('.box')[this.activeBox - 1].before(document.querySelectorAll('.box')[this.activeBox]);
-      this.numBufferBox = createPuzzle.randomArray[this.activeBox];
-      createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox - 1];
-      createPuzzle.randomArray[this.activeBox - 1] = this.numBufferBox;
-      this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
-    } else if (createPuzzle.randomArray[this.activeBox + createPuzzle.level] === 0) {
-      this.numBufferBox = this.activeBox;
-      document.querySelectorAll('.box')[this.activeBox + createPuzzle.level].before(document.querySelectorAll('.box')[this.activeBox]);
-      document.querySelectorAll('.box')[this.numBufferBox].before(document.querySelectorAll('.box')[this.numBufferBox + createPuzzle.level]);
-      this.numBufferBox = createPuzzle.randomArray[this.activeBox];
-      createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox
+        document.querySelectorAll('.box')[this.activeBox - 1].before(document.querySelectorAll('.box')[this.activeBox]);
+        this.numBufferBox = createPuzzle.randomArray[this.activeBox];
+        createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox - 1];
+        createPuzzle.randomArray[this.activeBox - 1] = this.numBufferBox;
+        this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
+      } else if (createPuzzle.randomArray[this.activeBox + createPuzzle.level] === 0) {
+        this.numBufferBox = this.activeBox;
+        document.querySelectorAll('.box')[this.activeBox + createPuzzle.level].before(document.querySelectorAll('.box')[this.activeBox]);
+        document.querySelectorAll('.box')[this.numBufferBox].before(document.querySelectorAll('.box')[this.numBufferBox + createPuzzle.level]);
+        this.numBufferBox = createPuzzle.randomArray[this.activeBox];
+        createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox
         + createPuzzle.level];
-      createPuzzle.randomArray[this.activeBox + createPuzzle.level] = this.numBufferBox;
-      this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
-    } else if (createPuzzle.randomArray[this.activeBox - createPuzzle.level] === 0) {
-      this.numBufferBox = this.activeBox;
-      document.querySelectorAll('.box')[this.activeBox - createPuzzle.level].after(document.querySelectorAll('.box')[this.activeBox]);
-      document.querySelectorAll('.box')[this.numBufferBox].after(document.querySelectorAll('.box')[this.numBufferBox - createPuzzle.level]);
-      this.numBufferBox = createPuzzle.randomArray[this.activeBox];
-      createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox
+        createPuzzle.randomArray[this.activeBox + createPuzzle.level] = this.numBufferBox;
+        this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
+      } else if (createPuzzle.randomArray[this.activeBox - createPuzzle.level] === 0) {
+        this.numBufferBox = this.activeBox;
+        document.querySelectorAll('.box')[this.activeBox - createPuzzle.level].after(document.querySelectorAll('.box')[this.activeBox]);
+        document.querySelectorAll('.box')[this.numBufferBox].after(document.querySelectorAll('.box')[this.numBufferBox - createPuzzle.level]);
+        this.numBufferBox = createPuzzle.randomArray[this.activeBox];
+        createPuzzle.randomArray[this.activeBox] = createPuzzle.randomArray[this.activeBox
         - createPuzzle.level];
-      createPuzzle.randomArray[this.activeBox - createPuzzle.level] = this.numBufferBox;
-      this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
+        createPuzzle.randomArray[this.activeBox - createPuzzle.level] = this.numBufferBox;
+        this.moveValue = +(document.querySelector('.move_value').textContent) + 1;
+      }
+      document.querySelector('.move_value').textContent = this.moveValue;
     }
-    document.querySelector('.move_value').textContent = this.moveValue;
+  }
+}
+
+let checkEmptyBox = true;
+function dragDrop(event) {
+  if (event.target.className !== 'box box_empty') {
+    move.activeBoxes(event);
+    const coorEmptyX = document.querySelector('.box_empty').getBoundingClientRect().left;
+    const coorEmptyY = document.querySelector('.box_empty').getBoundingClientRect().top;
+    const allBoxes = document.querySelectorAll('.box');
+    const boxOn = allBoxes[move.activeBox];
+    const boxOnCopy = boxOn.cloneNode(true);
+    boxOnCopy.classList.add('box_copy');
+    const shiftX = event.clientX - boxOn.getBoundingClientRect().left;
+    const shiftY = event.clientY - boxOn.getBoundingClientRect().top;
+    const boxWidth = boxOn.getBoundingClientRect().width / 2;
+    document.querySelector('.main_box').append(boxOnCopy);
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      boxOnCopy.style.left = `${pageX - shiftX}px`;
+      boxOnCopy.style.top = `${pageY - shiftY}px`;
+      if (pageX - shiftX > coorEmptyX - boxWidth && pageX - shiftX < coorEmptyX + boxWidth
+      && pageY - shiftY > coorEmptyY - boxWidth && pageY - shiftY < coorEmptyY + boxWidth) {
+        checkEmptyBox = true;
+      } else {
+        checkEmptyBox = false;
+      }
+      if (boxOn.getBoundingClientRect().left === (pageX - shiftX)
+    && boxOnCopy.getBoundingClientRect().top === pageY - shiftY) {
+        checkEmptyBox = true;
+      }
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    function removeEvent() {
+      document.querySelector('.main_box').removeChild(boxOnCopy);
+      boxOn.classList.remove('box_empty');
+      document.removeEventListener('mousemove', onMouseMove);
+      boxOnCopy.onmouseup = null;
+    }
+
+    boxOnCopy.addEventListener('mouseup', removeEvent);
+    boxOn.classList.add('box_empty');
+    boxOnCopy.ondragstart = function () {
+      return false;
+    };
   }
 }
 
 const move = new Move();
 
-document.querySelector('body').addEventListener('mousedown', move.activeBoxes);
-document.querySelector('body').addEventListener('mouseup', move.moveBoxes);
+createPuzzle.mainBox.addEventListener('mousedown', move.activeBoxes);
+createPuzzle.mainBox.addEventListener('mouseup', move.moveBoxes);
+createPuzzle.mainBox.addEventListener('mousedown', dragDrop);
 
 window.addEventListener('DOMContentLoaded', () => {
   createPuzzle.init();
