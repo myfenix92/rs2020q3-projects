acd718f856b441e85ad6b30c2d76068fc9970ed2
Yana Kornejkova <bloom.hog@gmail.com>
2020-11-28 00:19:58 +0300

    feat: delete CodeMirror.js, add function on click button

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -1,44 +1,78 @@
 import '../styles/index.scss';
-import * as CodeMirror from 'codemirror';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/ayu-dark.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/css/css';
-
+import { htmlText } from './htmlText';
 import { WriteHTML } from './Level';
 
-const myCodeMirror = CodeMirror.fromTextArea(document.getElementById('code'), {
-  lineNumbers: true,
-  matchBrackets: true,
-  mode: "text/html",
-  readOnly: true,
-  theme: "ayu-dark",
-  cursorHeight: 0,
+document.querySelector('.css_text').insertAdjacentHTML('afterbegin', '{ <br>/* Styles would go here. */<br>}');
+
+let level = htmlText.level_1.id;
+const writeHTML = new WriteHTML();
+const inputValue = document.querySelector('input');
+
+let isAnswer = true;
+let levelBuffer;
+
+document.querySelector('button').addEventListener('click', (e) => {
+  e.preventDefault();
+  isAnswer = true;
+  switch (inputValue.value) {
+    case '#sun':
+      levelBuffer = htmlText.level_2.id;
+      break;
+    case 'planet':
+      levelBuffer = htmlText.level_3.id;
+      break;
+    case 'planet moon':
+      levelBuffer = htmlText.level_4.id;
+      break;
+    case '.earth:visited':
+      levelBuffer = htmlText.level_5.id;
+      break;
+    case '.small':
+      levelBuffer = htmlText.level_6.id;
+      break;
+    case '.giant-group > planet':
+    case '.giant-group>planet':
+      levelBuffer = htmlText.level_7.id;
+      break;
+    case '.saturn[rings]':
+      levelBuffer = htmlText.level_8.id;
+      break;
+    case 'planet:not(.inhabitable)':
+      levelBuffer = htmlText.level_9.id;
+      break;
+    case 'planet:nth-child(5)':
+      levelBuffer = htmlText.level_10.id;
+      break;
+    case 'planet:last-child':
+      document.querySelectorAll('.circle').forEach((planet) => {
+        planet.classList.add('animation_stop');
+      });
+      break;
+    default:
+      isAnswer = false;
+      inputValue.value = '';
+      ufoError();
+      break;
+  }
+
+  if (isAnswer && level + 1 === levelBuffer) {
+    level = levelBuffer;
+    inputValue.value = '';
+    writeHTML.templateHTMLCode(level);
+    ufoWin();
+  }
 });
 
-const myCodeMirrorCSS = CodeMirror.fromTextArea(document.getElementById('css'), {
-  lineNumbers: true,
-  matchBrackets: true,
-  mode: "css",
-  indentUnit: 2,
-  readOnly: true,
-  theme: "material",
-  cursorHeight: 0,
-});
-myCodeMirrorCSS.setValue(`{\n\t/* Styles would go here. */\n}\n\n{\n\t/* Enter CSS selector and click 'Enter' or press Enter*/
-}\n\n\n\n\n\n\n\n\n\n\n\n\n`);
+function ufoError() {
+  document.querySelector('.ufo_answer').classList.remove('ufo_win');
+  document.querySelector('.ufo_answer').classList.add('ufo_error');
+}
 
-let i = 1;
-document.querySelectorAll('.CodeMirror-code > div').forEach((e) => {
-  e.setAttribute('id', i);
-  i += 1;
-});
-
-const writeHTML = new WriteHTML(2);
+function ufoWin() {
+  document.querySelector('.ufo_answer').classList.add('ufo_win');
+  document.querySelector('.ufo_answer').classList.remove('ufo_error');
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  writeHTML.templateHTMLCode();
-})
-
-export { myCodeMirror };
+  writeHTML.templateHTMLCode(level);
+});
