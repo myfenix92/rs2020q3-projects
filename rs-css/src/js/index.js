42c97c0e9d908eb2dd6321d17bd361028e8e4fc1
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-01 23:52:08 +0300

    feat: moved functions on click in helpers.js

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -1,78 +1,23 @@
 import '../styles/index.scss';
-import { htmlText } from './htmlText';
-import { WriteHTML } from './Level';
+import {
+  ENTER_BTN, RESET_BTN, HELP_BTN, INPUT, NAV_LEVEL, LAYOUT, HTML_VIEWER, writeHTML, inputAnswer,
+} from './constants';
+import {
+  chooseLevelNav, helpAnswer, resetGame, layoutMouseOver, layoutMouseOut,
+  htmlBlockMouseOver, htmlBlockMouseOut, clickEnterBtn,
+} from './helpers/helpers';
 
-document.querySelector('.css_text').insertAdjacentHTML('afterbegin', '{ <br>/* Styles would go here. */<br>}');
+NAV_LEVEL.addEventListener('click', chooseLevelNav);
+HELP_BTN.addEventListener('click', helpAnswer);
+RESET_BTN.addEventListener('click', resetGame);
+LAYOUT.addEventListener('mouseover', layoutMouseOver);
+LAYOUT.addEventListener('mouseout', layoutMouseOut);
+HTML_VIEWER.addEventListener('mouseover', htmlBlockMouseOver);
+HTML_VIEWER.addEventListener('mouseout', htmlBlockMouseOut);
 
-let level = htmlText.level_1.id;
-const writeHTML = new WriteHTML();
-const inputValue = document.querySelector('input');
-
-let isAnswer = true;
-let levelBuffer;
-
-document.querySelector('button').addEventListener('click', (e) => {
-  e.preventDefault();
-  isAnswer = true;
-  switch (inputValue.value) {
-    case '#sun':
-      levelBuffer = htmlText.level_2.id;
-      break;
-    case 'planet':
-      levelBuffer = htmlText.level_3.id;
-      break;
-    case 'planet moon':
-      levelBuffer = htmlText.level_4.id;
-      break;
-    case '.earth:visited':
-      levelBuffer = htmlText.level_5.id;
-      break;
-    case '.small':
-      levelBuffer = htmlText.level_6.id;
-      break;
-    case '.giant-group > planet':
-    case '.giant-group>planet':
-      levelBuffer = htmlText.level_7.id;
-      break;
-    case '.saturn[rings]':
-      levelBuffer = htmlText.level_8.id;
-      break;
-    case 'planet:not(.inhabitable)':
-      levelBuffer = htmlText.level_9.id;
-      break;
-    case 'planet:nth-child(5)':
-      levelBuffer = htmlText.level_10.id;
-      break;
-    case 'planet:last-child':
-      document.querySelectorAll('.circle').forEach((planet) => {
-        planet.classList.add('animation_stop');
-      });
-      break;
-    default:
-      isAnswer = false;
-      inputValue.value = '';
-      ufoError();
-      break;
-  }
-
-  if (isAnswer && level + 1 === levelBuffer) {
-    level = levelBuffer;
-    inputValue.value = '';
-    writeHTML.templateHTMLCode(level);
-    ufoWin();
-  }
-});
-
-function ufoError() {
-  document.querySelector('.ufo_answer').classList.remove('ufo_win');
-  document.querySelector('.ufo_answer').classList.add('ufo_error');
-}
-
-function ufoWin() {
-  document.querySelector('.ufo_answer').classList.add('ufo_win');
-  document.querySelector('.ufo_answer').classList.remove('ufo_error');
-}
+ENTER_BTN.addEventListener('click', clickEnterBtn);
+INPUT.addEventListener('keydown', clickEnterBtn);
 
 document.addEventListener('DOMContentLoaded', () => {
-  writeHTML.templateHTMLCode(level);
+  writeHTML.templateHTMLCode(inputAnswer.level);
 });
