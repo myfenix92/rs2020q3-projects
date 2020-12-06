6cc6b16172d4c765dbadc30db554c93a0d236d66
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-06 22:17:00 +0300

    feat: change imports, init, fix case

--- rs-css/src/js/Create Level/Level.js
+++ rs-css/src/js/Create Level/Level.js
@@ -1,9 +1,8 @@
-/* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
-import { htmlText } from '../helpers/htmlText';
-import { levelItem } from '../constants';
+import { levelItem } from '../constants/index';
+import { htmlText, getLocalStorageData, HelpersClass } from '../helpers/index';
 
-export const WriteHTML = class {
+export const CreateLevel = class {
   constructor() {
     this.htmlRoot = document.querySelector('.html_text');
     this.layoutInsert = document.querySelector('.layout_block');
@@ -12,20 +11,26 @@ export const WriteHTML = class {
 
   init(level) {
     // eslint-disable-next-line eqeqeq
-    if (localStorage.getItem('level_item') == undefined) {
+    if (getLocalStorageData('level') == undefined) {
       document.querySelector('.level_nav').insertAdjacentHTML('afterbegin', levelItem);
     } else {
-      document.querySelector('.level_nav').insertAdjacentHTML('afterbegin', localStorage.getItem('level_item'));
+      document.querySelector('.level_nav').insertAdjacentHTML('afterbegin', getLocalStorageData('level_item'));
     }
     document.querySelectorAll('.level_item').forEach((e) => {
       if (e.classList.contains('level_now')) {
         e.classList.remove('level_now');
       }
     });
+    document.querySelectorAll('.level_item').forEach((e) => {
+      if (e.classList.contains('wrong_answer')) {
+        e.classList.remove('right_answer');
+      }
+    });
     document.querySelectorAll('.level_item')[level - 1].classList.add('level_now');
-    this.layoutInsert.insertAdjacentHTML('beforeend', htmlText[level].layoutCode);
-    this.htmlRoot.insertAdjacentHTML('afterbegin', htmlText[level].text);
-    this.levelTextRoot.insertAdjacentHTML('afterbegin', htmlText[level].levelText);
+    this.layoutInsert.insertAdjacentHTML('beforeend', htmlText[`level_${level}`].layoutCode);
+    this.htmlRoot.insertAdjacentHTML('afterbegin', htmlText[`level_${level}`].text);
+    this.levelTextRoot.insertAdjacentHTML('afterbegin', htmlText[`level_${level}`].levelText);
+    document.querySelector('.facts_text').insertAdjacentHTML('afterbegin', htmlText[`level_${level}`].factsText);
   }
 
   templateHTMLCode(level) {
@@ -33,54 +38,45 @@ export const WriteHTML = class {
     this.htmlRoot.textContent = '';
     this.layoutInsert.textContent = '';
     this.levelTextRoot.textContent = '';
+    document.querySelector('.facts_text').textContent = '';
     this.init(level);
     document.querySelectorAll('.circle').forEach((planet) => {
       planet.classList.add('animation_stop');
     });
     switch (level) {
       case 1:
-        document.querySelector('.sun').classList.remove('animation_stop');
+        HelpersClass.classToggle('.sun');
         break;
       case 2:
-        document.querySelectorAll('.earth_group').forEach((planet) => {
-          planet.classList.remove('animation_stop');
-        });
+        HelpersClass.classesAllToggle('.earth_group');
         break;
       case 3:
-        document.querySelectorAll('.satellite').forEach((planet) => {
-          planet.classList.remove('animation_stop');
-        });
+        HelpersClass.classesAllToggle('.satellite');
         break;
       case 4:
-        document.querySelector('.earth').classList.remove('animation_stop');
+        HelpersClass.classToggle('.mercury');
+        HelpersClass.classToggle('.mars');
         break;
       case 5:
-        document.querySelector('.mercury').classList.remove('animation_stop');
-        document.querySelector('.mars').classList.remove('animation_stop');
+        HelpersClass.classToggle('.earth');
         break;
       case 6:
-        document.querySelectorAll('.giant_group').forEach((circle) => {
-          circle.classList.remove('animation_stop');
-        });
+        HelpersClass.classesAllToggle('.giant_group');
         break;
       case 7:
-        document.querySelector('.saturn').classList.remove('animation_stop');
+        HelpersClass.classToggle('.saturn');
         break;
       case 8:
-        document.querySelectorAll('.circle').forEach((planet) => {
-          planet.classList.remove('animation_stop');
-        });
-        document.querySelectorAll('.satellite').forEach((planet) => {
-          planet.classList.add('animation_stop');
-        });
-        document.querySelector('.sun').classList.add('animation_stop');
-        document.querySelector('.earth').classList.add('animation_stop');
+        HelpersClass.classesAllToggle('.circle');
+        HelpersClass.classesAllToggle('.satellite');
+        HelpersClass.classToggle('.sun');
+        HelpersClass.classToggle('.earth');
         break;
       case 9:
-        document.querySelector('.jupiter').classList.remove('animation_stop');
+        HelpersClass.classToggle('.jupiter');
         break;
       case 10:
-        document.querySelector('.neptune').classList.remove('animation_stop');
+        HelpersClass.classToggle('.neptune');
         break;
       default:
         break;
